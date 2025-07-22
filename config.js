3b672c7397fed17cb1671186f5b4bb5ba2e00063
Vignesh Peddi <vpeddi18@gmail.com>
2025-07-22 18:29:47 -0500

    ai chatbot

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  GEMINI_API_KEY: process.env.GEMINI_API_KEY || 'AIzaSyD10bZePC61wk5rSlaZO93ZQagLz7YDJ7w',
-  PORT: process.env.PORT || 3000
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  PORT: process.env.PORT
 }; 
